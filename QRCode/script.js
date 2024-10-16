19f9d0850d0e23cbe61331d997cccb006492b853
David Gilson <gilsond@gmail.com>
2024-10-16 20:32:03 +1000

    Restructure

--- QRCode/script.js
+++ QRCode/script.js
@@ -1,14 +1,41 @@
 window.onload = function() {
     const logo = document.getElementById('logo-upload');
     const canvas = document.getElementById('qr-canvas');
+    let iconFile;
     
     document.getElementById('URL').addEventListener('input', function(event) {
         logo.disabled = !isValidURL(event.target.value);
+        drawQR();
     });
     
     logo.addEventListener('change', function(event) {
-        const file = event.target.files[0];
-        if (file) {
+        iconFile = event.target.files[0];
+        drawQR();
+    });
+
+    document.getElementById('download-btn').addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.download = 'QRCode.png';
+        link.href = canvas.toDataURL('image/png');
+        link.click();
+    });
+
+    document.getElementById('icon-padding').addEventListener('change', function() {
+        drawQR();
+    });
+
+    function isValidURL(string) {
+        const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return !!pattern.test(string);
+    };
+
+    function drawQR () {
+        if (iconFile) {
             const reader = new FileReader();
             reader.onload = function(e) {
                 const logo = new Image();
@@ -39,24 +66,7 @@ window.onload = function() {
                 logo.src = e.target.result;
 
             };
-            reader.readAsDataURL(file);
+            reader.readAsDataURL(iconFile);
         }
-    });
-
-    document.getElementById('download-btn').addEventListener('click', function() {
-        const link = document.createElement('a');
-        link.download = 'QRCode.png';
-        link.href = canvas.toDataURL('image/png');
-        link.click();
-    });
-
-    function isValidURL(string) {
-        const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-        return !!pattern.test(string);
     }
 };
