d9b27d20b14e4e409434b9528b07fd8b9ccf81c0
David Gilson <gilsond@gmail.com>
2024-10-28 10:57:28 +1000

    split drawqr and drawicon

--- QRCode/script.js
+++ QRCode/script.js
@@ -6,7 +6,7 @@ window.onload = function () {
   const paddingInput = document.getElementById('icon-padding');
   // 1px x 1px transparent PNG image
   // const favIconDefault = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC"';
-  const favIconDefault = "icons/page.svg"
+  const favIconDefault = 'icons/page.svg';
   let iconFile;
   let url;
 
@@ -14,38 +14,42 @@ window.onload = function () {
     faviconImage.src = favIconDefault; // Fallback icon
   });
 
-  document.getElementById('URL').addEventListener('change', function (event) {
-    let url = event.target.value;
+  faviconImage.addEventListener('load', function (event) {
+    drawIcon();
+  });
+
+  document.getElementById('URL').addEventListener('change', function URLOnChange (event) {
+    url = event.target.value;
     // Checks if the URL has a specified protocol. If not, prepends "https://"
     if (!/^(https?:\/\/)/i.test(url)) {
-        url = 'https://' + url;
+      url = 'https://' + url;
     }
 
     console.log('URL:', url);
 
     if (isValidURL(url)) {
-        try {
-            const faviconUrl = getFaviconURL(url);  // No async/await needed here
-            faviconImage.src = faviconUrl;
-            logo.disabled = false;
+      try {
+        const faviconUrl = getFaviconURL(url); // No async/await needed here
+        faviconImage.src = faviconUrl;
+        logo.disabled = false;
 
-            drawQR();
-        } catch (error) {
-            console.error("Error setting favicon:", error);
-            faviconImage.src = favIconDefault;  // Use default icon if an error occurs
-        }
+        drawQR();
+      } catch (error) {
+        console.error('Error setting favicon:', error);
+        faviconImage.src = favIconDefault; // Use default icon if an error occurs
+      }
     } else {
-        logo.disabled = true;
-        faviconImage.src = favIconDefault;
+      logo.disabled = true;
+      faviconImage.src = favIconDefault;
     }
   });
 
   logo.addEventListener('change', function (event) {
     iconFile = event.target.files[0];
-    drawQR();
+    drawIcon();
   });
 
-  btnDownload.addEventListener('click', function () {
+  btnDownload.addEventListener('click', function btnDownloadClick () {
     const link = document.createElement('a');
     link.download = 'QRCode.png';
     link.href = canvas.toDataURL('image/png');
@@ -53,17 +57,17 @@ window.onload = function () {
   });
 
   paddingInput.addEventListener('change', function () {
-    drawQR();
+    drawIcon();
   });
 
   function isValidURL(string) {
     const pattern = new RegExp(
       '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$',
       'i'
     ); // fragment locator
     return !!pattern.test(string);
@@ -76,39 +80,50 @@ window.onload = function () {
   }
 
   function drawQR() {
+    const qr = new QRious({
+      element: canvas,
+      value: url,
+      size: 800,
+      padding: 30,
+    });
+
+    drawIcon();
+    btnDownload.disabled = false;
+  }
+
+  async function drawIcon() {
     if (iconFile) {
       const reader = new FileReader();
       reader.onload = function (e) {
-        const logo = new Image();
-        logo.onload = function () {
-          const qr = new QRious({
-            element: canvas,
-            value: url,
-            size: 800,
-            padding: 30,
-          });
-          const ctx = canvas.getContext('2d');
-          const logoSize = canvas.width / 5;
-          const logoX = (canvas.width - logoSize) / 2;
-          const logoY = (canvas.height - logoSize) / 2;
-          const padding = parseInt(paddingInput.value) || 4; // Ensure padding is a number
-
-          // Draw white rectangle with padding behind the logo
-          ctx.fillStyle = 'white';
-          ctx.fillRect(
-            logoX - padding,
-            logoY - padding,
-            logoSize + padding * 2,
-            logoSize + padding * 2
-          );
-          ctx.drawImage(logo, logoX, logoY, logoSize, logoSize);
-
-          btnDownload.disabled = false;
-        };
-        logo.src = e.target.result;
+        drawImage(e.target.result);
       };
       reader.readAsDataURL(iconFile);
-      canvas.title = `QR Code, scan to open: ${url}`;
+    } else if (faviconImage.src) {
+        const imgSrc = faviconImage.src;
+        drawImage(imgSrc);
     }
+  }
+
+  function drawImage(src) {
+    const logo = new Image();
+    logo.onload = function () {
+      const ctx = canvas.getContext('2d');
+      const logoSize = canvas.width / 5;
+      const logoX = (canvas.width - logoSize) / 2;
+      const logoY = (canvas.height - logoSize) / 2;
+      const padding = parseInt(paddingInput.value) || 4; // Ensure padding is a number
+
+      // Draw white rectangle with padding behind the logo
+      ctx.fillStyle = 'white';
+      ctx.fillRect(
+        logoX - padding,
+        logoY - padding,
+        logoSize + padding * 2,
+        logoSize + padding * 2
+      );
+      ctx.drawImage(logo, logoX, logoY, logoSize, logoSize);
+      canvas.title = `QR Code, scan to open: ${url}`;
+    };
+    logo.src = src;
   }
 };
